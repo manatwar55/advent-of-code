a04863f27e24157eb60f71e427da44b96f7147a0
manatwar55 <47839225+manatwar55@users.noreply.github.com>
2020-12-13 02:19:00 -0500

    Day 6

--- 2020/Day6/Custom Customs.py
+++ 2020/Day6/Custom Customs.py
@@ -1,4 +1,30 @@
+questions = {}
+
+def groupQuestions(data):
+    count = 0
+    everyone = 0
+    people = 0
+    for x in data:
+        if x != "":
+            d = dict.fromkeys(x)
+            for key, value in d.items():
+                questions[key] = questions.setdefault(key, 0) + 1
+            people += 1
+        else:
+            count += len(questions)
+            everyone += len([key for key, value in questions.items() if value == people])
+            people = 0
+            questions.clear()
+    count += len(questions)
+    everyone += len([key for key, value in questions.items() if value == people])
+    people = 0
+    questions.clear()
+    return(count, everyone)
 
 
 with open('input.txt', 'rt') as f:
         data = [line.strip() for line in f.readlines()]
+
+count, everyone = groupQuestions(data) 
+print(count)
+print(everyone)
